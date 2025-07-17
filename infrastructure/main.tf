d5776be14ee03c1eb60dfa627b59afcdb859e9b5
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-07-17 18:12:29 -0300

    Adjust deployment script

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -130,20 +130,20 @@ resource "null_resource" "deploy_app" {
       host        = module.ec2.ec2_instance_private_dns["app_springboot"]
       private_key = module.key_pair.private_key_pem
     }
-  }
 
-  inline = [
-    " echo 'Starting deployment of Spring Boot application...'",
-    " sudo yum update -y",
-    " sudo yum install java-17-amazon-corretto-headless -y",
-    " sudo kill -9 $(ps aux | grep java | awk '{print $2}') || true ",
-    " echo 'Starting S3 file copy...'",
-    " sudo mkdir -p /app",
-    " sudo chown -R ec2-user: /app",
-    " aws s3 cp s3://spring-boot-app-demo-bucket/deployments/demo-0.0.1-SNAPSHOT.jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar",
-    " echo 'Starting application...' ",
-    " nohup java -jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar >> /app/spring-boot-app-demo.log 2>&1 & disown",
-  ]
+    inline = [
+      " echo 'Starting deployment of Spring Boot application...'",
+      " sudo yum update -y",
+      " sudo yum install java-17-amazon-corretto-headless -y",
+      " sudo kill -9 $(ps aux | grep java | awk '{print $2}') || true ",
+      " echo 'Starting S3 file copy...'",
+      " sudo mkdir -p /app",
+      " sudo chown -R ec2-user: /app",
+      " aws s3 cp s3://spring-boot-app-demo-bucket/deployments/demo-0.0.1-SNAPSHOT.jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar",
+      " echo 'Starting application...' ",
+      " nohup java -jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar >> /app/spring-boot-app-demo.log 2>&1 & disown",
+    ]
+  }
 
   depends_on = [module.ec2]
 
