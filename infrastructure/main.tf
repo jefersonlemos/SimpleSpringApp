114d764a6fc688983b99fb79ac74b82a6b057ed9
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-07-17 11:40:00 -0300

    Adjust deployment script

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -137,9 +137,11 @@ resource "null_resource" "deploy_app" {
       " sudo yum install java-17-amazon-corretto-headless -y",
       " sudo kill -9 $(ps aux | grep java | awk '{print $2}') || true ",
       " echo 'Starting S3 file copy...'",
-      " sudo mkdir -p /app && sudo aws s3 cp s3://spring-boot-app-demo-bucket/deployments/demo-0.0.1-SNAPSHOT.jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar",
+      " sudo mkdir -p /app",
+      " sudo chown -R ec2-user: /app",
+      " aws s3 cp s3://spring-boot-app-demo-bucket/deployments/demo-0.0.1-SNAPSHOT.jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar",
       " echo 'Starting application...' ",
-      " sudo nohup java -jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar > /app/spring-boot-app-demo.log 2>&1 &"
+      " nohup java -jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar >> /app/spring-boot-app-demo.log 2>&1 &"
     ]
   }
 
