aa05a058acf5ae1f35d635c9b8b53417704cd262
Jeferson Lemos <jefersonlemosdossantos@gmail.com>
2025-07-17 22:22:07 -0300

    Adjust deployment script

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'Starting deployment of Spring Boot application...'
-sudo yum update -y && yum install java-17-amazon-corretto-headless -y
+sudo yum update -y && sudo yum install java-17-amazon-corretto-headless -y
 sudo kill -9 $(ps aux | grep java | awk '{print $2}') || true
 echo 'Starting S3 file copy...'
-sudo mkdir -p /app && chown -R ec2-user: /app
+sudo mkdir -p /app && sudo chown -R ec2-user: /app
 aws s3 cp s3://spring-boot-app-demo-bucket/deployments/demo-0.0.1-SNAPSHOT.jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar
 echo 'Starting application...'
 setsid java -jar /app/spring-boot-app-demo-0.0.1-SNAPSHOT.jar >> /app/spring-boot-app-demo.log 2>&1 < /dev/null &
